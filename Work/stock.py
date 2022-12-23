1fb38de76a357c14fa277401529dc2badbd1a625
sirnathan00 <sirnathan00@aol.com>
2022-12-23 14:46:37 +0100

    finsihed chapter 5

--- Work/stock.py
+++ Work/stock.py
@@ -1,18 +1,32 @@
 class Stock :
+    __slots__ = ('name', '_shares', 'price')
     def __init__(self, name, shares, price):
         self.name = str(name)
-        self.shares = int(shares)
+        self.shares = shares
         self.price = float(price)
+    
+    @property
+    def shares(self):
+        return self._shares
+
+    @property
+    def cost(self) :
+        '''
+        this def returns the total cost of a stock, now with the property tag
+        '''
+        return float(self.shares * self.price)    
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('expected int')
+        self._shares = value
     
     def __repr__(self):
         return f'Stock(\'{self.name}\', {self.shares}, {self.price})'
 
     #this function give back how much a share costs
-    def cost(self) :
-        '''
-        this def returns the total cost of a stock
-        '''
-        return float(self.shares * self.price)
+
     
     #this function sells shares giving a new share indication
     def sell(self, sell_shares=int):
--- Work/testfilechapter5.py
+++ Work/testfilechapter5.py
@@ -1,43 +1,58 @@
 from stock import Stock, NewStock
 
-# print(s.__dict__)
-# print(Stock.__dict__)
-# print(s.__class__)
-goog = Stock('GOOG', 100, 490.1)
-ibm = Stock('IBM', 50, 91.23)
-# print(goog.__dict__)
-# print(ibm.__dict__)
-goog.date = '6/11/2007'
-# print(goog.__dict__)
-# print(ibm.__dict__)
-goog.__dict__['time'] = '9:45am'
-# print(goog.time)
-# print(goog.cost())
-# print(ibm.cost())
-# print(str(Stock.__dict__['cost'](goog)))
-# print(str(Stock.__dict__['cost'](ibm)))
-Stock.foo = 42
-# print(goog.foo, ibm.foo)
-# print(goog.__dict__)
-s = goog.sell
-# print(s)
-s(25)
-# print(s)
-# print(s.__func__)
-# print(s.__self__)
-s.__func__(s.__self__, 25)
-# print(goog.shares)
+# # print(s.__dict__)
+# # print(Stock.__dict__)
+# # print(s.__class__)
+# goog = Stock('GOOG', 100, 490.1)
+# ibm = Stock('IBM', 50, 91.23)
+# # print(goog.__dict__)
+# # print(ibm.__dict__)
+# goog.date = '6/11/2007'
+# # print(goog.__dict__)
+# # print(ibm.__dict__)
+# goog.__dict__['time'] = '9:45am'
+# # print(goog.time)
+# # print(goog.cost())
+# # print(ibm.cost())
+# # print(str(Stock.__dict__['cost'](goog)))
+# # print(str(Stock.__dict__['cost'](ibm)))
+# Stock.foo = 42
+# # print(goog.foo, ibm.foo)
+# # print(goog.__dict__)
+# s = goog.sell
+# # print(s)
+# s(25)
+# # print(s)
+# # print(s.__func__)
+# # print(s.__self__)
+# s.__func__(s.__self__, 25)
+# # print(goog.shares)
 
-n = NewStock('ACME', 50, 123.45)
-# print(n.cost())
-# n.yow()
-# print(NewStock.__bases__)
-# print(NewStock.__mro__)
+# n = NewStock('ACME', 50, 123.45)
+# # print(n.cost())
+# # n.yow()
+# # print(NewStock.__bases__)
+# # print(NewStock.__mro__)
 
-for cls in n.__class__.__mro__:
-    if 'cost' in cls.__dict__:
-        print(cls)
-        break
+# # for cls in n.__class__.__mro__:
+# #     if 'cost' in cls.__dict__:
+# #         print(cls)
+# #         break
 
-print(cls.__dict__['cost'])
+# # print(cls.__dict__['cost'])
     
+# s = Stock('IBM', 50, 91.1)
+# print(s.shares)
+# s.shares = 75
+# print(s.shares)
+
+# p = Stock('Test',  100, 'test')
+s = Stock('GOOG', 100, 490.1)
+print(s.cost)
+
+# a= s.cost
+# print(a)
+
+s.price= 385.15
+print(s.price)
+s.prices = 410.2

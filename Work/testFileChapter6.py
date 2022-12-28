84286840216cbe8d53e7f7c26a86376d336e3a59
sirnathan00 <sirnathan00@aol.com>
2022-12-28 16:20:45 +0100

    chapter 6.2 test

--- Work/testFileChapter6.py
+++ Work/testFileChapter6.py
@@ -27,12 +27,47 @@ import report
 # # exercise 6.2 above this
 
 # import report
-# portfolio = report.read_portfolio('C:\\Users\\natha\\OneDrive\\Desktop\\practical-python\\Work\\Data\\portfolio.csv')
+portfolio = report.read_portfolio('C:\\Users\\natha\\OneDrive\\Desktop\\practical-python\\Work\\Data\\portfolio.csv')
 # print(portfolio)
 # print(len(portfolio))
 # print(portfolio[0])
 # print(portfolio[1])
 # print(portfolio[0:3])
-# print('IBM' in portfolio)
+print('IBM' in portfolio)
 # print('AAPL' in portfolio)
 # # finished chapter 6.1 above
+
+def countdown(n):
+    print('counting down from ', n)
+    while n > 0:
+        yield n
+        n -= 1
+
+# x = countdown(10)
+# print(x)
+# print(x.__next__())
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+# print(next(x))
+
+# # above this is the litrature for chapter 6.2
+
+def filematch(filename, substr):
+    with open(filename, 'r') as f:
+        for line in f:
+            if substr in line:
+                yield line
+
+for line in open('C:\\Users\\natha\\OneDrive\\Desktop\\practical-python\\Work\\Data\\portfolio.csv'):
+    print(line, end=' ')
+
+print('\n \n \n ')
+for line in filematch('C:\\Users\\natha\\OneDrive\\Desktop\\practical-python\\Work\\Data\\portfolio.csv', 'IBM'):
+    print(line, end=' ')
